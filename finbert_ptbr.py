0f6e55086308c6291cef42d42e11928be52719c9
santluan <luan.santos14@outlook.com>
2026-02-06 20:06:06 -0300

    add models

--- finbert_ptbr.py
+++
@@ -1,8 +0,0 @@
-from transformers import (AutoTokenizer, BertForSequenceClassification, AutoModelForSequenceClassification, pipeline)
-from sklearn.metrics import classification_report
-import pandas as pd
-# import warnings
-# warnings.filterwarnings("ignore")
-
-pipe = pipeline("text-classification", model="lucas-leme/FinBERT-PT-BR")
-print(pipe(['Hoje a bolsa caiu', 'Hoje a bolsa subiu']))
